7d41c963d41e951b588923d987515c77d3bf04d8
unknown <delphinel@gmail.com>
2015-05-01 16:14:07 +0200

    xgboost3

--- classifier.py
+++ classifier.py
@@ -11,32 +11,29 @@ class Classifier(BaseEstimator):
     def __init__(self):
         self.label_encoder = LabelEncoder()
         self.scaler = StandardScaler()
-        self.clf1 = None
-        self.clf2 = None
+        self.clf = None
         self.param = {'eval_metric':'mlogloss'}
         self.param['num_class'] = 9
-        self.param['subsample'] = 0.9        
-        self.num_round = 120 
-        self.obj1 = 'multi:softprob'
-        self.obj2 = 'multi:softmax'
+        param['subsample'] = 0.795
+        param['gamma'] = 0.9        
+        self.num_round = 170
+        self.obj = 'multi:softprob'
  
     def fit(self, X, y):        
         X = self.scaler.fit_transform(X.astype(np.float32))              
         y = self.label_encoder.fit_transform(y).astype(np.int32)
         dtrain = xgb.DMatrix( X, label=y.astype(np.float32))
         
-        self.param['objective'] = self.obj1  
-        self.clf1 = xgb.train(self.param, dtrain, self.num_round)  
-
-        self.param['objective'] = self.obj2  
-        self.clf2 = xgb.train(self.param, dtrain, self.num_round)
+        self.param['objective'] = self.obj  
+        self.clf = xgb.train(self.param, dtrain, self.num_round)  
  
     def predict(self, X):
-        X = self.scaler.fit_transform(X.astype(np.float32))
+        X = self.scaler.transform(X.astype(np.float32))
         dtest = xgb.DMatrix(X)       
-        return self.clf2.predict(dtest)
+        label_index_array = np.argmax(self.clf.predict(dtest), axis=1)
+        return self.label_encoder.inverse_transform(label_index_array)
  
     def predict_proba(self, X):
-        X = self.scaler.fit_transform(X.astype(np.float32))
+        X = self.scaler.transform(X.astype(np.float32))
         dtest = xgb.DMatrix(X)
-        return self.clf1.predict(dtest)
+        return self.clf.predict(dtest)
