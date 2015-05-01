bfc9cfc998c0a8dd2daf69d7efe5432ebc4b2479
unknown <delphinel@gmail.com>
2015-05-01 19:24:55 +0200

    ada_rectify

--- classifier.py
+++ classifier.py
@@ -1,39 +1,67 @@
-from sklearn.preprocessing import StandardScaler
 from sklearn.base import BaseEstimator
-from sklearn.preprocessing import LabelEncoder
-import numpy as np
-
-#import sys
-#sys.path.append('C:\\src\c++\\xgboost\\wrapper')
-import xgboost as xgb
-
-class Classifier(BaseEstimator):
-    def __init__(self):
-        self.label_encoder = LabelEncoder()
-        self.scaler = StandardScaler()
-        self.clf = None
-        self.param = {'eval_metric':'mlogloss'}
-        self.param['num_class'] = 9
-        self.param['subsample'] = 0.795
-        self.param['gamma'] = 0.9        
-        self.num_round = 170
-        self.obj = 'multi:softprob'
  
-    def fit(self, X, y):        
-        X = self.scaler.fit_transform(X.astype(np.float32))              
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+ 
+import theano
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne.layers import DropoutLayer
+from lasagne.nonlinearities import softmax
+from lasagne.nonlinearities import rectify
+from lasagne.updates import nesterov_momentum
+from lasagne.updates import adagrad
+from nolearn.lasagne import NeuralNet
+ 
+ 
+class Classifier(BaseEstimator):
+ 
+    def __init__(self):
+        self.net = None
+        self.label_encoder = None
+ 
+    def fit(self, X, y):
+        layers0 = [('input', InputLayer),
+                   ('dense0', DenseLayer),
+                   ('dropout', DropoutLayer),
+                   ('dense1', DenseLayer),
+                   ('output', DenseLayer)]
+        X = X.astype(theano.config.floatX)
+        self.label_encoder = LabelEncoder()
         y = self.label_encoder.fit_transform(y).astype(np.int32)
-        dtrain = xgb.DMatrix( X, label=y.astype(np.float32))
-        
-        self.param['objective'] = self.obj  
-        self.clf = xgb.train(self.param, dtrain, self.num_round)  
+        self.scaler = StandardScaler()
+        X = self.scaler.fit_transform(X)
+        num_classes = len(self.label_encoder.classes_)
+        num_features = X.shape[1]
+        self.net = NeuralNet(layers=layers0,
+                             input_shape=(None, num_features),
+                             dense0_num_units=300,
+                             dense0_nonlinearity=rectify,
+                             dropout_p=0.5,
+                             dense1_num_units=200,
+                             dense1_nonlinearity=rectify,
+                             output_num_units=num_classes,
+                             output_nonlinearity=softmax,
+ 
+                             update=adagrad,
+                             update_learning_rate=0.02,
+ 
+                             eval_size=0.2,
+                             verbose=1,
+                             max_epochs=100,
+                             )
+        self.net.fit(X, y)
+        return self
  
     def predict(self, X):
-        X = self.scaler.transform(X.astype(np.float32))
-        dtest = xgb.DMatrix(X)       
-        label_index_array = np.argmax(self.clf.predict(dtest), axis=1)
-        return self.label_encoder.inverse_transform(label_index_array)
+        X = X.astype(theano.config.floatX)
+        X = self.scaler.fit_transform(X)
+        return self.label_encoder.inverse_transform(self.net.predict(X))
  
     def predict_proba(self, X):
-        X = self.scaler.transform(X.astype(np.float32))
-        dtest = xgb.DMatrix(X)
-        return self.clf.predict(dtest)
+        X = X.astype(theano.config.floatX)
+        X = self.scaler.fit_transform(X)
+        return self.net.predict_proba(X)
